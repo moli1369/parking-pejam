25091cda9eccc04e7e7e8f9940bf45d0ea83073a
Muhammad <31498696+moli1369@users.noreply.github.com>
2026-08-23 02:15:32 +0200

    feat(iot): persist users sensors and readings

--- src/ParkingPejam.Infrastructure/ParkingDbContext.cs
+++ src/ParkingPejam.Infrastructure/ParkingDbContext.cs
@@ -7,6 +7,9 @@ public sealed class ParkingDbContext(DbContextOptions<ParkingDbContext> options)
 {
     public DbSet<ParkingSpot> ParkingSpots => Set<ParkingSpot>();
     public DbSet<ParkingEvent> ParkingEvents => Set<ParkingEvent>();
+    public DbSet<User> Users => Set<User>();
+    public DbSet<ParkingSensor> ParkingSensors => Set<ParkingSensor>();
+    public DbSet<ParkingSensorReading> ParkingSensorReadings => Set<ParkingSensorReading>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -27,10 +30,32 @@ public sealed class ParkingDbContext(DbContextOptions<ParkingDbContext> options)
             entity.Property(x => x.OldStatus).HasConversion<string>().HasMaxLength(32);
             entity.Property(x => x.NewStatus).HasConversion<string>().HasMaxLength(32);
             entity.HasIndex(x => x.TimestampUtc);
-            entity.HasOne(x => x.ParkingSpot)
-                .WithMany()
-                .HasForeignKey(x => x.ParkingSpotId)
-                .OnDelete(DeleteBehavior.Cascade);
+            entity.HasOne(x => x.ParkingSpot).WithMany().HasForeignKey(x => x.ParkingSpotId).OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.Username).HasMaxLength(64).IsRequired();
+            entity.HasIndex(x => x.Username).IsUnique();
+            entity.Property(x => x.PasswordHash).HasMaxLength(512).IsRequired();
+            entity.Property(x => x.Role).HasMaxLength(32).IsRequired();
+        });
+
+        modelBuilder.Entity<ParkingSensor>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.ExternalId).HasMaxLength(128).IsRequired();
+            entity.HasIndex(x => x.ExternalId).IsUnique();
+            entity.Property(x => x.DeviceKey).HasMaxLength(512).IsRequired();
+            entity.HasOne(x => x.ParkingSpot).WithMany().HasForeignKey(x => x.ParkingSpotId).OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<ParkingSensorReading>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => new { x.ParkingSensorId, x.ReceivedAtUtc });
+            entity.HasOne(x => x.ParkingSensor).WithMany().HasForeignKey(x => x.ParkingSensorId).OnDelete(DeleteBehavior.Cascade);
         });
     }
 }
